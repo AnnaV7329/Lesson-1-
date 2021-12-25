314ba6d147677ac5369d4fc821419a15b6762495
Anna Vilskaya <vilskayaa@mail.ru>
2021-12-25 11:30:55 +0300

    Добавили решение десятого примера

---
+++ Example0010_find/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1,62,73,44,52,76,47,38};
+
+int n = array.Length;
+int find = 38;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index +1;
+    index++;
+}
